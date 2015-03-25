3f2d1b65dbe07c209e81aa5d8ee2f0432aa3e38a
Ondrej Kosarko <kosarko@ufal.mff.cuni.cz>
2015-03-25 11:47:25 +0100

    the DS login initiator will be removed

--- aai.js
+++ aai.js
@@ -34,7 +34,7 @@
           opts.ourEntityID,
           opts.responseUrl,
           [ ],
-          opts.host + '/Shibboleth.sso/DS?SAMLDS=1&target='+opts.target+'&entityID=');
+          opts.host + '/Shibboleth.sso/Login?SAMLDS=1&target='+opts.target+'&entityID=');
         djc.metadata = [opts.metadataFeed];
         djc.subtitle = "Login via Your home institution (e.g. university)";
 	djc.textHelp = opts.textHelp;
